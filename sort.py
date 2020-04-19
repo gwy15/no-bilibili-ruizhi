961d2841eb5cc4e93e6164ae5e9c597bdcf31c54
gwy15 <gwy15thu@gmail.com>
2020-04-19 13:53:44 +0800

    update

--- sort.py
+++ sort.py
@@ -3,29 +3,40 @@ import re
 from collections import defaultdict
 
 
-def type_of(line: str) -> str:
-    return re.search('">([tru])=', line).group(0)
+class Filter:
+    def __init__(self, s: str):
+        result = re.search(r'">([tru])=([^<]+)<', s)
+        if result is None:
+            raise RuntimeError(f'{s} parse failed.')
+        self.type = result.group(1)
+        self.content = result.group(2)
+        print(self.type, self.content)
+
+    def xml(self) -> str:
+        return f'    <item enabled="true">{self.type}={self.content}</item>\n'
 
 
 p = Path('tv.bilibili.player.xml')
 with p.open(encoding='utf8') as f:
     lines = f.readlines()
-lines = map(
-    lambda l: l.strip(),
-    lines
-)
+lines = map(lambda l: l.strip(), lines)
 lines = filter(
     lambda l: len(l) > 0 and 'filters' not in l,
     lines
 )
-lines_by_type = defaultdict(list)
-for line in lines:
-    lines_by_type[type_of(line)].append(line)
+filters = map(Filter, lines)
+filters_by_type = defaultdict(list)
+for filter in filters:
+    filters_by_type[filter.type].append(filter)
 
 with p.open('w', encoding='utf8') as f:
     f.write('<filters>\n')
-    for t, lines in lines_by_type.items():
-        for line in lines:
-            f.write(f'    {line}\n')
+    for type in filters_by_type:
+        filters = sorted(
+            filters_by_type[type],
+            key=lambda f: f.content
+        )
+        for filter in filters:
+            f.write(filter.xml())
         f.write('\n')
     f.write('</filters>\n')
